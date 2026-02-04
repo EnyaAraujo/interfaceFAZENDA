1e617c0df1c970dd3de8d7654d34b94959587993
Enya <enya.clara1246@gmail.com>
2026-02-04 13:06:00 -0300

    Update App.java

--- src/fazenda/main/App.java
+++ src/fazenda/main/App.java
@@ -5,9 +5,9 @@ import javax.swing.SwingUtilities;
 
 public class App {
     public static void main(String[] args) {
-        // Boa prática: Iniciar a GUI na Thread de eventos do Swing
+        
         SwingUtilities.invokeLater(() -> {
-            new JanelaPrincipal(); // Chama a janela visual
+            new JanelaPrincipal();
         });
     }
 }
